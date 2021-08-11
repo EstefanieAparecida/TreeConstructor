7436f6cb5f042a7b41d38a5545d7540846938e3d
Estefanie <Estefanieaparecida0@gmail.com>
2021-08-11 14:50:51 -0300

    Termino da construção da arvore

--- src/main/java/com/binary/Tree/Arvore.java
+++ src/main/java/com/binary/Tree/Arvore.java
@@ -1,37 +1,70 @@
 package com.binary.Tree;
 
 public class Arvore<Tipo extends Comparable> {
-	
+
 	private Elemento<Tipo> raiz;
-	
+
 	public Arvore() {
-		 this.raiz = null;
+		this.raiz = null;
 	}
-	
+
 	public void adicionar(Tipo valor) {
-		Elemento<Tipo> novoElemento = new Elemento<Tipo>(valor); 
-			if (raiz == null) {
-				this.raiz = novoElemento;
-			}else {
-				Elemento<Tipo> atual = this.raiz;
-				while(true) {
-					if (novoElemento.getValor().compareTo(atual.getValor())== -1) {
-						if(atual.getEsquerda() != null) {
-							atual = atual.getEsquerda();
-						}else {
-							atual.setEsquerda(novoElemento);
-							break;
-						}
-					}else {//se for maior ou igual
-						if(atual.getDireita() != null) {
-							atual = atual.getDireita();
-						}else {
-							atual.setDireita(novoElemento);
-							break;
-						}
+		Elemento<Tipo> novoElemento = new Elemento<Tipo>(valor);
+		if (raiz == null) {
+			this.raiz = novoElemento;
+		} else {
+			Elemento<Tipo> atual = this.raiz;
+			while (true) {
+				if (novoElemento.getValor().compareTo(atual.getValor()) == -1) {
+					if (atual.getEsquerda() != null) {
+						atual = atual.getEsquerda();
+					} else {
+						atual.setEsquerda(novoElemento);
+						break;
+					}
+				} else {// se for maior ou igual
+					if (atual.getDireita() != null) {
+						atual = atual.getDireita();
+					} else {
+						atual.setDireita(novoElemento);
+						break;
 					}
 				}
 			}
+		}
+
 	}
 
+	public Elemento<Tipo> getRaiz() {
+		return raiz;
+	}
+
+	public void emOrdem(Elemento<Tipo> atual) {
+		if (atual != null) {
+			emOrdem(atual.getEsquerda());
+			System.out.println(atual.getValor());
+			emOrdem(atual.getDireita());
+
+		}
+
+	}
+
+	public void preOrdem(Elemento<Tipo> atual) {
+		if (atual != null) {
+			System.out.println(atual.getValor());
+			preOrdem(atual.getEsquerda());
+			preOrdem(atual.getDireita());
+
+		}
+
+	}
+
+	public void posOrdem(Elemento<Tipo> atual) {
+		if (atual != null) {
+			posOrdem(atual.getEsquerda());
+			posOrdem(atual.getDireita());
+			System.out.println(atual.getValor());
+		}
+
+	}
 }
--- src/main/java/com/binary/Tree/Main.java
+++ src/main/java/com/binary/Tree/Main.java
@@ -5,8 +5,18 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Arvore<Integer> arvore = new Arvore<Integer>();
-		arvore.adicionar(10); 
-		arvore.adicionar(5);
+		arvore.adicionar(2); 
+		arvore.adicionar(1);
+		arvore.adicionar(7);
+		arvore.adicionar(4);
+		System.out.println("Em Ordem: ");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		System.out.println("Pré Ordem: ");
+		arvore.preOrdem(arvore.getRaiz());
+		
+		System.out.println("Pós Ordem: ");
+		arvore.posOrdem(arvore.getRaiz());
 	}
 
 }
